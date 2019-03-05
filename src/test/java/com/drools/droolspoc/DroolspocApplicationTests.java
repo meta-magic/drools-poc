8ff64cb90707a5a394cab470fc79c5dfcfbc2b17
ketangote <ketan.gote@metamagic.in>
2019-03-05 15:55:49 +0530

    Drools POC

---
+++ src/test/java/com/drools/droolspoc/DroolspocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drools.droolspoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DroolspocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
